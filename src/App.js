982b6361bc2cba04770c7c09124b43e8a48a9583
Julio García Pérez <julio.garciaperez@telefonica.com>
2021-07-28 20:18:58 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import './App.css';
 import Navbar from './components/navbar/Navbar';
-import { Route, Switch } from 'react-router-dom';
+import { Redirect, Route, Switch } from 'react-router-dom';
 import Home from './components/home/Home';
 import Contact from './components/contact/Contact';
 import About from './components/about/About';
@@ -13,13 +12,15 @@ function App() {
 
       <div className="container-fluid p-5">
         <Switch>
-          <Route exact path="/" component={Home} />
+          <Route exact path="/home" component={Home} />
 
           <Route exact path="/contact" component={Contact} />
 
           <Route exact path="/about" component={About} />
 
           <Route exact path="/users/:id/detail" component={UserDetail} />
+
+          <Redirect to="/home"/>
         </Switch>
       </div>
     </div>
